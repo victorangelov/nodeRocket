f92bf303f2f03a75747e11256260177a02240f70
Victor Silva <victor_angelo@hotmail.com>
2021-02-17 09:03:30 -0300

    update
    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,47 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'legal essa merda World'});
+/**
+ * Método HTTPS:
+ * 
+ * GET: Bucar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PAT: alterar inforamção no back-end
+ * put todos os dados, pat, algo específico
+ * DELETE: deletar informações do back-end
+ */
+
+ //método get, para puxar informações do backend
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ]);
 });
+//método post, para criar informações no back-end
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ])
+})
+//método post, para alterar informações no back-end precisa ter o id na rota
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 3',
+    ])
+})
+//método delete, para deletar informações no back-end precisa ter o id na rota
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+    ])
+})
 
 app.listen(3333, () => {
     console.log(' 🚀 Back-end iniciado!')
